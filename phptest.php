21de7389a52300420096f04bf32ec940e5272ade
Derek Royse <derekroyse@gmail.com>
2019-01-03 14:08:54 -0500

    Connecting database.

--- phptest.php
+++ phptest.php
@@ -6,9 +6,9 @@
     <?php echo '<p>Hello World</p>';
 
     // In the variables section below, replace user and password with your own MySQL credentials as created on your server
-    $servername = "localhost";
-    $username = "RTAdmin";
-    $password = "rogers001";
+    $servername = "heroku_f0b2bb985121ebe";
+    $username = "bf97bd4916868e";
+    $password = "797882dd";
 
     // Create MySQL connection
     $conn = mysqli_connect($servername, $username, $password);
