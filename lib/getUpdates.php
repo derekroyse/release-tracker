eeeba2ec96dbba73e25d26f7cd490be21bdf939f
Derek Royse <derekroyse@gmail.com>
2019-01-24 15:27:49 -0500

    More work with IGDB api.

--- lib/getUpdates.php
+++ lib/getUpdates.php
@@ -1,5 +1,5 @@
 <?php
-		// Report errors while testing.
+	// Report errors while testing.
 		// TODO: Remove this.
 		error_reporting(E_ALL);
 		ini_set('display_errors', '1');
@@ -7,79 +7,100 @@
 		// Master array that holds all media types
 		$master_array = array();  
 
-		// Get Movie API Data
-		// Todo: Handle multiple pages
-		$curl = curl_init();
+	// Get Movie API Data
+		$page_num = 1;
+		while ($page_num < 51){
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => "https://api.themoviedb.org/3/discover/movie?api_key=4bfcf1281ed7338461685e64a5cc0f3c&language=en-US&region=US&sort_by=primary_release_date.asc&include_adult=false&include_video=false&page=" . $page_num . "&primary_release_date.gte=2019-01-14",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_POSTFIELDS => "{}",
+			));
 
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://api.themoviedb.org/3/discover/movie?api_key=4bfcf1281ed7338461685e64a5cc0f3c&language=en-US&region=US&sort_by=primary_release_date.asc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2019-01-14",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => "",
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "GET",
-			CURLOPT_POSTFIELDS => "{}",
-		));
-
-		$response_json = json_decode(curl_exec($curl))->results;
-		
-		// Add each title to master array.
-		foreach($response_json as $movie){
-			$new_array = array('id' => 'mv-' . $movie -> id,
-												'title' => $movie -> title,
-												'release_date' => $movie -> release_date,
-												'type' => 'Movie (Theatrical Release)',
-												'platform' => '');
-			array_push($master_array, $new_array);
+			$response = curl_exec($curl);
+			if (isset(json_decode($response)->results)){
+				$response_json = json_decode($response)->results;
+			} else {
+				break;
+			}
+			
+			// Add each title to master array.
+			foreach($response_json as $movie){
+				$new_array = array('id' => 'mv-' . $movie -> id,
+													'title' => $movie -> title,
+													'release_date' => $movie -> release_date,
+													'type' => 'Movie (Theatrical Release)',
+													'platform' => '');
+				array_push($master_array, $new_array);
+			}
+			$page_num++;
 		}
 
 		$err = curl_error($curl);
-
 		curl_close($curl);
 
-		// Get VG API Data
-		// Todo: Handle multiple pages
-		$curl = curl_init();
+	// Get VG API Data
+		$offset = 0;
+		while ($offset < 2501){
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => "https://api-v3.igdb.com/games?fields=id,name,platforms,release_dates.*&limit=50&filter[release_dates.date][gt]=" . time() . "&offset=" . $offset,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_POSTFIELDS => "",
+				CURLOPT_HTTPHEADER => array(
+					"Accept: application/json",
+					"Postman-Token: d2c754b0-78a4-452e-9ac7-e18e03bed008",
+					"cache-control: no-cache",
+					"user-key: c219b1070d2cdfcef8a9cf7bebeca311"
+				),
+			));
 
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://api-v3.igdb.com/games?fields=id,name,platforms,release_dates.*&limit=50&filter[release_dates.date][gt]=" . time(),
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => "",
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "GET",
-			CURLOPT_POSTFIELDS => "",
-			CURLOPT_HTTPHEADER => array(
-				"Accept: application/json",
-				"Postman-Token: d2c754b0-78a4-452e-9ac7-e18e03bed008",
-				"cache-control: no-cache",
-				"user-key: c219b1070d2cdfcef8a9cf7bebeca311"
-			),
-		));
+			$vg_data = curl_exec($curl);
+			$vg_decoded = json_decode($vg_data);
 
-		// Error handling due to SQL exception on API side.
-		// TODO: Revisit and find permanent fix.
-		$vg_fixed = substr(curl_exec($curl), 0, -97);
-		$vg_test = json_decode($vg_fixed);
+			// Add each title to master array.
+			foreach($vg_decoded as $vg){
+				$counter = 1;				
+				// For each release date:
+				if (isset($vg -> release_dates)){
+					foreach ($vg -> release_dates as $date ){
+						
+						if (isset($date -> date)){
+							$release_date = date('Y-m-d', $date -> date);
+						} else if (isset($date-> human)){
+							$release_date = $date -> human;
+						} else {
+							$release_date = 'TBD';
+						}
 
-		// Add each title to master array.
-		foreach($vg_test as $vg){
-			if (isset($vg -> release_dates[0] -> date) ){
-				// For each release platform:
-				foreach ($vg -> release_dates as $date ){
-					$new_array = array('id' => 'vg-' . $vg -> id . '-' . $date -> platform,
-													'title' => $vg -> name,
-													'release_date' => date('Y-m-d', $date -> date),
-													'type' => 'Video Game',
-													'platform' => $date -> platform);
-					array_push($master_array, $new_array);          
+						$new_array = array('id' => 'vg-' . $vg -> id . '-' . $counter,
+														'title' => $vg -> name,
+														'release_date' => $release_date,
+														'type' => 'Video Game',
+														'platform' => $date -> platform);
+						array_push($master_array, $new_array);
+						$counter++;
+					}
 				}
 			}
+
+			$offset += 50;
 		}
 
-		// Return data (testing)...
+		$err = curl_error($curl);
+		curl_close($curl);
+
+	// Return data (testing)...
 		// $returnArray = new stdClass();
 		// $returnArray->data = $master_array;
 		// $response = json_encode($returnArray);
@@ -93,7 +114,7 @@
 		// 	echo $response;
 		// }
 
-		//... or save data to DB (production)
+	//... or save data to DB (production)
 		// In the variables section below, replace user and password with your own MySQL credentials as created on your server
 		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 		if (array_key_exists("host", $url)){
@@ -111,7 +132,10 @@
 		}
 
 		$sql = "INSERT INTO titles(id, title, release_date, platform, type)
-			VALUES ( ?, ?, ?, ?, ? )";
+			VALUES ( ?, ?, ?, ?, ? )
+			ON DUPLICATE KEY UPDATE 
+				title = VALUES(title),
+				release_date = VALUES(release_date)";
 
 		$query = $conn->prepare($sql);
 
@@ -138,7 +162,7 @@
 			echo $rows_added . ' titles added successfully!';
 		}	
 
-		 echo "<pre>";
+		echo "<pre>";
     print_r($master_array);
     echo "</pre>";
 
--- views/list/master.php
+++ views/list/master.php
@@ -21,6 +21,9 @@
 <script type="text/javascript">  
   $(document).ready(function() {
     var releaseTable = $('#releaseTable').dataTable({
+        aLengthMenu: [
+        [25, 50, 100, 200, -1],
+        [25, 50, 100, 200, "All"]],
         "processing": true,
         "ajax": {
             "url": "/lib/masterList.php",
